d1689154c176ada0e1e153541930b7b8d9dbbda8
italovieiradj-Zeep <italovieiradj@gmail.com>
2026-05-15 17:13:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,30 @@
 import express from 'express';
-const cors = require('cors');
-require('dotenv').config();
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+import playersRoutes from './routes/players.js';
+import playlistsRoutes from './routes/playlists.js';
+import sacRoutes from './routes/sac.js';
+import ttsRoutes from './routes/tts.js';
+import billingRoutes from './routes/billing.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-import playersRoutes from './routes/players.js';
-app.use('/players', playersRoutes);
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
-app.use('/api/players', require('./routes/players'));
-app.use('/api/playlists', require('./routes/playlists'));
-app.use('/api/sac', require('./routes/sac'));
-app.use('/api/tts', require('./routes/tts'));
-app.use('/api/billing', require('./routes/billing'));
+app.use('/players', playersRoutes);
+app.use('/playlists', playlistsRoutes);
+app.use('/sac', sacRoutes);
+app.use('/tts', ttsRoutes);
+app.use('/billing', billingRoutes);
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Zeep Backend v2 rodando na porta ${port}`);
 });

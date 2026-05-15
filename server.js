45f4e39d7c1ebc1246601441f08652d0e00611c4
italovieiradj-Zeep <italovieiradj@gmail.com>
2026-05-15 16:58:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+import playersRoutes from './routes/players.js';
+app.use('/players', playersRoutes);
 app.use(express.urlencoded({ extended: true }));
 
 // Rotas
